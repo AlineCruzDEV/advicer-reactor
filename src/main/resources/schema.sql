eede9262ee9c5a77137808ff912feceae21cf428
Juliana Aquino <juliana.aquino@f1rst.com.br>
2023-03-03 18:56:49 -0300

    Adding sql commands for users.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 create database advices_db;
 create schema public;
+
 create table advices (
     id serial PRIMARY KEY,
     advice VARCHAR NOT NULL
@@ -7,3 +8,10 @@ create table advices (
 insert into advices (advice) VALUES ('O melhor conselho que se pode dar a um semelhante é um bom exemplo!');
 insert into advices (advice) VALUES ('Persista, insista e não desista!');
 insert into advices (advice) VALUES ('Se conselho fosse bom, ninguém dava, vendia!');
+
+create table users (
+    id serial PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
+insert into users (name, email) VALUES ('nome1', 'email1@email.com');
